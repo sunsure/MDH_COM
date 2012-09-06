aca4d2e8e77b8c35dbe87d417e79409048f51087
Mark Holmberg <mark@custombit.com>
2012-09-06 12:39:55 -0600

    Initial Commit.

---
+++ app/controllers/my_controller.rb
@@ -0,0 +1,4 @@
+class MyController < ApplicationController
+  def dashboard
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mdh::Application.config.secret_token = 'fdd4666f9d707ae8f0365ddc1c507ca49b0d7dfbdf23e77f5ffe871ce79cf61e97fc9c70f3125e6fefb31be6879f635932ea606fa7f36dee34f240d80e1d75a2'
