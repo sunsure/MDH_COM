02328c959628798383212603c2276b41d0b6fbab
Mark Holmberg <mark@custombit.com>
2012-09-20 15:14:53 -0600

    Stub out some Sessions Controller tests.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,15 +2,30 @@ require 'spec_helper'
 
 describe SessionsController do
 
-  describe "GET :new" do
-    it "returns http success" do
-      get :new
-      response.should be_success
+  describe "successful login" do
+    describe "GET :new" do
+      it "returns http success" do
+        get :new
+        response.should be_success
+      end
+
+      it "should render the new template" do
+        get :new
+        response.should render_template(:new)
+      end
     end
 
-    it "should render the new template" do
-      get :new
-      response.should render_template(:new)
+    it "should find the right user" do
+      correct_user = User.find_by_email("test@example.com")
+    end
+
+    it "should find the right user regardless of case" do
+    end
+
+    it "should set the cookie" do
+    end
+
+    it "should redirect to the root url" do
     end
   end
 
