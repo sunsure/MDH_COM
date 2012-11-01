205f3cce5f04de7275d89954ddbe0f528b60ef3b
Mark Holmberg <mark@custombit.com>
2012-11-01 15:04:52 -0600

    Markdown Helper. Refs #12.
    * The markdown helper will now check to make sure it was given text.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,19 +9,21 @@ module ApplicationHelper
   end
 
   def to_markdown(text)
-    options = {
-      autolink: true,
-      hard_wrap: true,
-      filter_html: true,
-      no_intra_emphasis: true,
-      fenced_code_blocks: true,
-      lax_html_blocks: true,
-      strikethrough: true,
-      superscript: true,
-      tables: true,
-    }
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
-    markdown.render(text).html_safe
+    if text.present?
+      options = {
+        autolink: true,
+        hard_wrap: true,
+        filter_html: true,
+        no_intra_emphasis: true,
+        fenced_code_blocks: true,
+        lax_html_blocks: true,
+        strikethrough: true,
+        superscript: true,
+        tables: true,
+      }
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+      markdown.render(text).html_safe
+    end
   end
 
 end
