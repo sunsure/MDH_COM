51d7a7a6370e02a54d203eb0101c3e5dbcc4483d
Mark Holmberg <mark.d.holmberg@gmail.com>
2013-02-01 23:23:03 -0700

    Prevents apostrophes in User factory last names
    Because it screws the tests up when they have one

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :user do
     first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
+    last_name { Faker::Name.last_name.gsub(/'/,'') }
     sequence(:email) { |n| "foo#{n}@example.com" }
     password { "foobar" }
     password_confirmation { "foobar" }
