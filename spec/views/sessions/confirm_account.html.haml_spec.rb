6e0a12c6b624cd8da84a5b1cc85917736f4ff737
Mark Holmberg <mark.d.holmberg@gmail.com>
2013-02-01 23:27:31 -0700

    Normalizes page headers to use h3 tag
    Because it shouldn't be an h1 tag!

--- spec/views/sessions/confirm_account.html.haml_spec.rb
+++ spec/views/sessions/confirm_account.html.haml_spec.rb
@@ -5,7 +5,7 @@ describe "sessions/confirm_account" do
   it "renders the confirm_account form" do
     render
 
-    assert_select "h1", text: "Account Confirmation"
+    assert_select "h3", text: "Account Confirmation"
     assert_select "p.lead", text: "Why am I seeing this page?"
 
     assert_select "form", action: confirm_create_path, method: :post do
