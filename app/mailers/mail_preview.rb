6c034f6b8f780390dde9d5f50c990620ebef504f
Mark Holmberg <mark.d.holmberg@gmail.com>
2013-02-11 18:02:58 -0700

    Only enables MailPreview in development mode
    Because it's crashing heroku

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -1,18 +1,20 @@
-class MailPreview < MailView
+if Rails.env.development?
+  class MailPreview < MailView
 
-  # User confirmation email
-  def confirmation
-    @user = User.last
-    @user.confirm_token = "12345"
-    mail = UserMailer.confirmation(@user)
+    # User confirmation email
+    def confirmation
+      @user = User.last
+      @user.confirm_token = "12345"
+      mail = UserMailer.confirmation(@user)
+    end
+
+    # User resetting their password
+    def reset_password
+      @user = User.last
+      @user.password_reset_token = "12345"
+      @user.password_reset_sent_at = Time.zone.now
+      mail = UserMailer.reset_password(@user)
+    end
+
   end
-
-  # User resetting their password
-  def reset_password
-    @user = User.last
-    @user.password_reset_token = "12345"
-    @user.password_reset_sent_at = Time.zone.now
-    mail = UserMailer.reset_password(@user)
-  end
-
 end
