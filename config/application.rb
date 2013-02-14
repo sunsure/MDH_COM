f22b1ab8a9030966bbb64da2b467ba9045a1cfd4
Mark Holmberg <mark.d.holmberg@gmail.com>
2013-02-13 18:58:53 -0700

    Testing if heroku still needs to prevent initialize_on_precompile

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Mdh
     config.autoload_paths += %W(#{config.root}/app/uploaders)
 
     # Try to fix heroku's crap
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
