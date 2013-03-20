08d4ccacf86423945021a2d4c680f6c3f92bb999
Mark Holmberg <mark.d.holmberg@gmail.com>
2013-03-19 18:09:17 -0600

    Fixes asset_path problem when generating Open Graph Meta Tags. #59.
    Because heroku is stupid with asset_path when they're not precompiled.

--- app/models/article.rb
+++ app/models/article.rb
@@ -49,7 +49,7 @@ class Article < ActiveRecord::Base
     if icon.present?
       result << "<meta property='og:image' content='#{icon.url(:thumb)}'/>"
     else
-      result << "<meta property='og:image' content='#{ActionController::Base.helpers.asset_path("favicon.png")}'/>"
+      result << "<meta property='og:image' content='/images/favicon.png'/>"
     end
     result << "<meta property='og:title' content='#{title}'/>"
     result << "<meta property='og:url' content='#{Rails.application.routes.url_helpers.article_url(to_param, host:  Rails.application.config.super_cool_mailer_host)}'/>"
